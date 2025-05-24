88000d68d7c760eb867c7845aaf848b2aa8173b7
marianaakuma <153663415+marianaakuma@users.noreply.github.com>
2025-05-24 00:34:27 +0000

    4

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
-from flask import flask , request,jsonify
+from flask import Flask, request, jsonify
 
-app = flask(__name__)
+app = Flask(__name__)
+
+@app.route('/test', methods=['GET', 'POST'])
+def test():
+    if request.method == "GET":
+        return jsonify({"response": "Get Request Called"})
+    elif request.method == "POST":
+        req_Json = request.json
+        name = req_Json['name']
+        return jsonify({"response": "Hi " + name})
+
+if __name__ == '__main__':
+    app.run(debug=True, port=9090)
+      
